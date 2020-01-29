efaecd2a252030289ecb5261e356330dae1ac550
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-01-29 14:24:17 -0500

    sfwa

--- javascript/practiceExcercises.js
+++ javascript/practiceExcercises.js
@@ -8,18 +8,10 @@ console.log(y);
 // 2. Print the Fibonacci number that is greater than 1000.
 n = 0;
 l = 1;
-<<<<<<< HEAD
-while(n <== 1000){
-  n = n + l;
-  l = l + 1;
-  console.log(l);
-
-=======
 While(n <== 1000){
   n = n + l;
   l = l + n;
   console.log(1);
-  
->>>>>>> 536eaa3904d2138154ac6cd596506751231d00d0
+
 }
 //Print the number of integers from 1 through 100 that are divisible by 3 or 5 but not both
