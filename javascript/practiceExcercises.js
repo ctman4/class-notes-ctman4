19563c070d579117fe8ce05bbe5e4ec58608766f
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-02-10 12:15:32 -0500

    BOE Website final touches

--- javascript/practiceExcercises.js
+++ javascript/practiceExcercises.js
@@ -44,3 +44,45 @@ Books.shared = function(author){
 }
 
 //3. Define a function that works like the range function in Python. For example, range(5) returns [0,1,2,3,4] and range(3,8) returns [3,4,5,6,7].
+
+//////////////////////////////////////////////////////////////////////////////////// Exercise #1
+
+// Create an array of objects representing the courses you are taking this semester.
+const courses = [
+  {subject: 'CS', number: 220},
+  {subject: 'CS', number: 332},
+  {subject: 'MATH', number: 315},
+  {subject: 'MUS', number: 100}
+
+];
+
+// Iterate through this array to find the largest course number in your schedule.
+
+let largest = 0;
+for (const course of courses){
+  if (course.number > largest){
+    largest = course.number;
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////// Testing #1
+
+console.log(largest);
+//////////////////////////////////////////////////////////////////////////////////// Exercise #2
+
+// Define a constructor function for Book objects.
+
+
+// Define a shared method for books that lets you check whether a given author was one of the authors in this book.
+
+
+//////////////////////////////////////////////////////////////////////////////////// Testing #2
+
+
+
+//////////////////////////////////////////////////////////////////////////////////// Exercise #3
+
+// Define a function that works like the range function in Python.
+
+
+//////////////////////////////////////////////////////////////////////////////////// Testing #3
