a7580aeae3320d7a07d35389c93027d8902bc1c0
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-02-12 13:56:26 -0500

    Promises for parallel asynch calls

--- mongo/faculty/create.js
+++ mongo/faculty/create.js
@@ -1,57 +1,38 @@
 // Store some data in the faculty database
 
 const mongoose = require('mongoose');
-
 const connect = require('./db');
-
 const Professor = require('./schema');
 
-connect();// connect to the database
-//any commands before the connection are added to the cue, not asynchronous in this specific case
+connect(); // To the database
 
-//all commands in the database are asynchronous
-
+// Create some faculty
 const harcourt = new Professor({
   name: 'Ed Harcourt',
   rank: 'Full',
   started: 2003,
-  courses:[140, 220, 345]
+  courses: [140, 220, 345, 362, 364]
 });
 
 const torrey = new Professor({
   name: 'Lisa Torrey',
   rank: 'Associate',
   started: 2009,
-  courses:[140, 219, 332, 362, 374, 380]
+  courses: [140, 219, 332, 362, 374, 380]
 });
 
 const lee = new Professor({
   name: 'Choong-Soo Lee',
   rank: 'Associate',
   started: 2010,
-  courses:[140, 219, 256, 321, 370]
+  courses: [140, 219, 256, 321, 370]
 });
 
-
-// Delete any previous data
-mongoose.connection.dropDatabase(function() {
-
-  // Save the new data
-  harcourt.save(function(error) {
-    if (error) console.error(error.stack);
-
-    torrey.save(function(error) {
-      if (error) console.error(error.stack);
-
-      lee.save(function(error) {
-        if (error) console.error(error.stack);
-
-        //We do this if (error) to make sure they save before closing (it is asynchronous)
-        // Disconnect
-        mongoose.connection.close(function() {
-          console.log('Database is ready.');
-        });
-      });
-    });
-  });
-});
+// Reset the data
+mongoose.connection.dropDatabase()
+  .then(() => harcourt.save())
+  .then(() => torrey.save())
+  .then(() => lee.save())
+  .then(() => mongoose.connection.close())
+  .then(() => console.log('Database is ready.'))
+  .catch(error => console.error(error.stack));
